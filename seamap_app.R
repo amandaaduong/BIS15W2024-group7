ae15941e9ec441ed399808c05ed2b0ea4d6d6ae7
jsmnh <hsyhsiao@ucdavis.edu>
2024-03-09 21:39:55 -0800

    Seamap app R script update

---
+++ seamap_app.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(shiny)
+library(shinydashboard)
+library(leaflet)
+library(dplyr)
+library(readr)
+
+seamap <- read_csv("data/obis_seamap_swot_65e0e9344b011_20240229_153235_site_locations_csv.csv")
+
+ui <- dashboardPage(
+  dashboardHeader(title = "Sea Turtle Biogeography", titleWidth = 400),
+  dashboardSidebar(
+    selectInput("country", "Select Country:", choices = unique(seamap$country), selected = "Australia")
+  ),
+  dashboardBody(
+    leafletOutput("map", width = "100%", height = "600px")
+  )
+)
+
+server <- function(input, output) {
+  filtered_data <- reactive({
+    filter(seamap, country == input$country)
+  })
+  
+  output$map <- renderLeaflet({
+    center_lat <- mean(filtered_data()$latitude)
+    center_lon <- mean(filtered_data()$longitude)
+    
+    leaflet() %>%
+      addTiles(urlTemplate = "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+               attribution = '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a> contributors') %>%
+      setView(lng = center_lon, lat = center_lat, zoom = 4) %>%
+      addMarkers(data = filtered_data(),
+                 lng = ~longitude, lat = ~latitude,
+                 popup = ~paste("Location: ", country))
+  })
+}
+
+shinyApp(ui, server)
